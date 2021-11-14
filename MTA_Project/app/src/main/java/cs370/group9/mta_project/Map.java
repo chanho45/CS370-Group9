648db73b1378070db149119b70821df40b0d8c8c
ChanHo Lee <45chanho@gmail.com>
2021-11-14 14:33:51 -0800

    set up MS Bing Map SDK

--- MTA_Project/app/src/main/java/cs370/group9/mta_project/Map.java
+++ MTA_Project/app/src/main/java/cs370/group9/mta_project/Map.java
@@ -1,18 +1,7 @@
 package cs370.group9.mta_project;
 
-import com.microsoft.maps.Geopoint;
-import com.microsoft.maps.MapAnimationKind;
-import com.microsoft.maps.MapElementLayer;
-import com.microsoft.maps.MapIcon;
-import com.microsoft.maps.MapImage;
-import com.microsoft.maps.MapProjection;
-import com.microsoft.maps.MapRenderMode;
-import com.microsoft.maps.MapScene;
-import com.microsoft.maps.MapStyleSheet;
-import com.microsoft.maps.MapStyleSheets;
-import com.microsoft.maps.MapTappedEventArgs;
 import com.microsoft.maps.MapView;
 
 public class Map {
-
+    private MapView map;
 }
