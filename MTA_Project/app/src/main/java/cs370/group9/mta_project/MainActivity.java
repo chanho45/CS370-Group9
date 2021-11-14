abff186d1cac521f0017c49d8a2408b265184382
ChanHo Lee <45chanho@gmail.com>
2021-11-14 13:49:31 -0800

    Create new Android Studio Project

---
+++ MTA_Project/app/src/main/java/cs370/group9/mta_project/MainActivity.java
@@ -0,0 +1,15 @@
+package cs370.group9.mta_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
