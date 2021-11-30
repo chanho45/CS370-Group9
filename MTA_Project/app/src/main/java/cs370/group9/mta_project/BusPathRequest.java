b82a1ecdcbd4bfcd2a5be399d666299fd4d7bfa2
ChanHo Lee <45chanho@gmail.com>
2021-11-29 20:17:03 -0800

    add prototype path request and route search

---
+++ MTA_Project/app/src/main/java/cs370/group9/mta_project/BusPathRequest.java
@@ -0,0 +1,88 @@
+package cs370.group9.mta_project;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.maps.android.PolyUtil;
+import com.microsoft.maps.Geopoint;
+import com.microsoft.maps.MapPolyline;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BusPathRequest {
+    private static final String URL_ENDPOINT
+            ="http://bustime.mta.info/api/where/stops-for-route/{bus_id}.json"
+            +"?key=" + BuildConfig.MTA_KEY
+            +"&includePolylines=true" + "&version=2";
+
+    static class RoutePath{
+        MapPolyline polyline;
+        List<BusStop> stops;
+    }
+    interface Callback{
+        void onSuccess(RoutePath result);
+        void onFailure();
+    }
+
+    static void sendRequest(Context context, String bus_id, Callback callback){
+        if(bus_id==null){
+            Toast.makeText(context, "Invalid route id", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        RequestQueue queue = Volley.newRequestQueue(context);
+        queue.add(new StringRequest(
+                Request.Method.GET,
+                URL_ENDPOINT.replace("{bus_id}", bus_id),
+                (String res) -> {
+                    RoutePath result = parse(res);
+                    if(result == null){
+                        callback.onFailure();
+                        return;
+                    }
+                    callback.onSuccess(result);
+                },
+                (VolleyError error) -> callback.onFailure()
+        ));
+    }
+
+    private static RoutePath parse(String json){
+        RoutePath result = new RoutePath();
+
+        try{
+            JSONObject responseJObj = new JSONObject(json);
+            JSONObject dataJObj = responseJObj.getJSONObject("data");
+            JSONObject entryJObj = dataJObj.getJSONObject("entry");
+
+            JSONArray polylineJArr = entryJObj.getJSONArray("polylines");
+            List<Geopoint> points = new ArrayList<Geopoint>();
+            for(int i = 0; i < polylineJArr.length(); i++){
+                JSONObject polylineJObj = polylineJArr.getJSONObject(i);
+                String polyString = polylineJObj.getString("points");
+
+                List<LatLng> polyLatLons = PolyUtil.decode(polyString);
+                for(LatLng latLng: polyLatLons){
+                    points.add(LatLng2Geopoint(latLng));
+                }
+            }
+        }catch (Exception e){
+            return null;
+        }
+
+        return result;
+    }
+
+    private static Geopoint LatLng2Geopoint(LatLng latLng){
+        return new Geopoint(latLng.latitude, latLng.longitude);
+    }
+}
---
+++ MTA_Project/app/src/main/java/cs370/group9/mta_project/BusRouteSearch.java
@@ -0,0 +1,88 @@
+package cs370.group9.mta_project;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BusRouteSearch {
+    private static final String NYCT_ENDPOINT
+            = "http://bustime.mta.info/api/where/route-ids-for-agency/MTA NYCT.json"
+            + "?key=" + BuildConfig.MTA_KEY;
+    private static final String MTABC_ENDPOINT
+            = "http://bustime.mta.info/api/where/route-ids-for-agency/MTABC.json"
+            + "?key=" + BuildConfig.MTA_KEY;
+
+    interface Callback {
+        void onSuccess(List<String> results);
+        void onFailure();
+    }
+
+    static void sendRequest(Context context, String query, Callback callback){
+        if (query == null || query.isEmpty()) {
+            Toast.makeText(context, "Invalid query", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        RequestQueue queue =  Volley.newRequestQueue(context);
+        // check if asked bus route is in MTA NYCT
+        queue.add(new StringRequest(
+                Request.Method.GET,
+                NYCT_ENDPOINT,
+                (String response) -> {
+                    List<String> results = parse(response, query);
+
+                    // do MTABC too
+                    queue.add(new StringRequest(
+                            Request.Method.GET,
+                            MTABC_ENDPOINT,
+                            (String ABCres) -> {
+                                results.addAll(parse(ABCres, query));
+
+                                if (results == null || results.isEmpty()) {
+                                    callback.onFailure();
+                                    return;
+                                }
+                                callback.onSuccess(results);
+                            },
+                            (VolleyError error) -> callback.onFailure()
+                    ));
+                },
+                (VolleyError error) -> callback.onFailure()
+        ));
+    }
+
+    private static List<String> parse(String json, String query){
+        List<String> result = new ArrayList<String>();
+
+        try{
+            JSONObject resJObj = new JSONObject(json);
+            JSONObject dataJObj = resJObj.getJSONObject("data");
+            JSONArray listJArr = dataJObj.getJSONArray("list");
+
+            for(int i = 0; i < listJArr.length(); i++){
+                // if exact match, send it to top
+                if(listJArr.getString(i).equals(query)){
+                    result.add(0, listJArr.getString(i));
+                } // otherwise, add it to the bottom
+                else if(listJArr.getString(i).contains(query)){
+                    result.add(listJArr.getString(i));
+                }
+            }
+        }catch (Exception e){
+            return null;
+        }
+
+        return result;
+    }
+}
