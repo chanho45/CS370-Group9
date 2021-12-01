80fe029ad107fc8a2f2fcfe3eb39c20d58d5b49d
ChanHo Lee <45chanho@gmail.com>
2021-11-30 17:01:35 -0800

    add reading icon from res

--- MTA_Project/app/src/main/java/cs370/group9/mta_project/Bus.java
+++ MTA_Project/app/src/main/java/cs370/group9/mta_project/Bus.java
@@ -1,17 +1,28 @@
 package cs370.group9.mta_project;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 import com.microsoft.maps.Geopoint;
 import com.microsoft.maps.MapIcon;
+import com.microsoft.maps.MapImage;
+
 
 public class Bus extends MapIcon {
     private final BusRoute route;
 
-    public Bus(BusRoute route, Geopoint location){
+    public Bus(BusRoute route, Geopoint location, Context context){
         super();
         this.route=route;
 
         this.setLocation(location);
         this.setTitle(route.getRouteNumber());
+
+        Bitmap icon = ColorChanger.changeBitmapColor(
+                BitmapFactory.decodeResource(context.getResources(), R.drawable.bus),
+                route.getColor());
+        this.setImage(new MapImage(icon));
     }
 
     public BusRoute getRoute() {
--- MTA_Project/app/src/main/java/cs370/group9/mta_project/BusStop.java
+++ MTA_Project/app/src/main/java/cs370/group9/mta_project/BusStop.java
@@ -1,18 +1,26 @@
 package cs370.group9.mta_project;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 import com.microsoft.maps.Geopoint;
 import com.microsoft.maps.MapIcon;
+import com.microsoft.maps.MapImage;
 
 public class BusStop extends MapIcon {
     private BusRoute[] routes;
     private String id;
 
-    public BusStop(String name, String id, BusRoute[] routes, Geopoint location){
+    public BusStop(String name, String id, BusRoute[] routes, Geopoint location, Context context){
         super();
         this.id = id;
         this.routes = routes;
 
         this.setLocation(location);
         this.setTitle(name);
+
+        Bitmap icon = BitmapFactory.decodeResource(context.getResources(), R.drawable.stop);
+        this.setImage(new MapImage(icon));
     }
 }
