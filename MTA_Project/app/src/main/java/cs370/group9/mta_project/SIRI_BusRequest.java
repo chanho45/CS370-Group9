9c0e755ebb5a4b5bd7e1f0dbdc1c77ace5040d9e
ChanHo Lee <45chanho@gmail.com>
2021-12-01 21:20:14 -0800

    create SIRI realtime request file

---
+++ MTA_Project/app/src/main/java/cs370/group9/mta_project/SIRI_BusRequest.java
@@ -0,0 +1,4 @@
+package cs370.group9.mta_project;
+
+public class SIRI_BusRequest {
+}
---
+++ MTA_Project/app/src/main/java/cs370/group9/mta_project/SIRI_StopRequest.java
@@ -0,0 +1,8 @@
+package cs370.group9.mta_project;
+
+public class SIRI_StopRequest {
+
+    static class StopMonitor{
+
+    }
+}
